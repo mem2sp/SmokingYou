34f59aecccbbe6e966a8734bf6299e1e31829b50
bodyaant <memetosuper@gmail.com>
2026-07-03 14:59:26 +0300

    app version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = getGitCommitCount()
-        versionName = "1.0.1"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
